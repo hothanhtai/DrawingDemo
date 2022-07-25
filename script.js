1ef8637e46889aa310afa2c26b7242c416ba3694
HoThanhTai <88653907+hothanhtai@users.noreply.github.com>
2022-07-25 11:36:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -52,4 +52,30 @@ function drawLine(x1,y1,x2,y2){
     ctx.lineWidth = size * 2
     ctx.stroke()
 }
+function updateSizeOnScreen() {
+    sizeEL.innerText = size
+}
 
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
