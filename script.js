6599968750bfebcca928ebef28f77a84c0994fc1
HoThanhTai <88653907+hothanhtai@users.noreply.github.com>
2022-07-25 11:42:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const changeColor = document.getElementById('color')
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
 
 
 
